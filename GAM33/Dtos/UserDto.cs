8e07fea243b204fd3e078881f773a565cd40838c
Fady Melad <fmelad86@gmail.com>
2025-04-30 16:47:50 +0300

    Make Interfaces of Auth

---
+++ GAM33/Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GAM33.Dtos
+{
+    public class UserDto
+    {
+        public required string DisplayName { get; set; }
+
+        public required string Email { get; set; }
+        public required string Token { get; set; }
+    }
+}
---
+++ Gma33.Core/Interfaces/IAuthServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gma33.Apis.Dtos;
+
+
+namespace Gma33.Core.Interfaces
+{
+    public interface IAuthServices
+    {
+    }
+}
